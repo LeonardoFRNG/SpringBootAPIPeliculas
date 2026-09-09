7a5fe09735d2418688885fd1d62f9de353c240d8
LeonardoFRNG <leonardojim321@gmail.com>
2026-09-09 14:11:09 -0500

    d

---
+++ src/test/java/com/peliculas/play/PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peliculas.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
