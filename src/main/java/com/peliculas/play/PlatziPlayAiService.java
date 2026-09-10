c20ed172ec9de8cd3e7add96f4c2fde7a5ed3663
LeonardoFRNG <leonardojim321@gmail.com>
2026-09-09 20:40:33 -0500

    .

--- src/main/java/com/peliculas/play/PlatziPlayAiService.java
+++ src/main/java/com/peliculas/play/PlatziPlayAiService.java
@@ -7,9 +7,10 @@ import dev.langchain4j.service.UserMessage;
 public interface PlatziPlayAiService {
 
     @UserMessage ("""
-                Genera un saludo de bienvenida a la plataforma de prueba: RiwiPlay.
-                Contexto: es una plataforma de cine y peliculas.
-                Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
+                Genera un saludo de bienvenida a la plataforma de prueba: RiwiSpace.
+                Contexto: es una plataforma de aprendizaje dedicado a la astronomia, en cada actualizacion daras un dato curioso diferente acerca del espacio que enganche al publico,
+                Cambia los emojis en cada actualizacion, usa tematica de astronomia.
+                Usa menos de 400 caracteres y hazlo con el estilo de space x.
             """)
     String generateGreeting();
 }
